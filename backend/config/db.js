8e03f6e0611f4f9e6d9db72968b66ff8d51d487a
PabodaFdo <pabodafernandowpsf@gmail.com>
2026-07-01 19:27:01 +0530

    configure MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB connected');
+        console.log('MongoDB connected successfully');
     } catch (error) {
-        console.error('MongoDB connection failed');
+        console.error('MongoDB connection failed:', error.message);
         process.exit(1);
     }
 };
